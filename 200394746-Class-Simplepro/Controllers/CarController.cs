ba185bf9e2a482e32763e7c1475e180a9d8f60f3
harsh24895 <harshpatel24895@gmail.com>
2019-05-27 20:01:48 -0400

    Fetch the Specific car details.

---
+++ 200394746-Class-Simplepro/Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿
+using _200394746_Class_Simplepro.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _200394746_Class_Simplepro.Controllers
+{
+    public class CarController : Controller
+    {
+
+        List<Car> carlist = new List<Car>()
+        {
+            new Car{Id=1, Brand ="Toyota",Model="Corolla",Type="Compact",Year=2017},
+            new Car{Id=2, Brand ="Ford",Model="F-1150",Type="Truck",Year=2018},
+            new Car{Id=3, Brand ="Dodge",Model="Caravan",Type="Minivan",Year=2019}
+
+        };
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View(carlist);
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id > carlist.Count)
+            {
+                return Content("Invalid Car id");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carlist[ind];
+            return View(car);
+
+        }
+    }
+}
---
+++ 200394746-Class-Simplepro/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200394746_Class_Simplepro.Models
+{
+    public class Car
+    {
+
+            public int Id { get; set; }
+            public String Brand { get; set; }
+            public String Model { get; set; }
+            public String Type { get; set; }
+            public int Year { get; set; }
+
+        }
+    
+}
